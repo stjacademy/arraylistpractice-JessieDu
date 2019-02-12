1daa10286fe170fc81ae5ce72e93dda7fd833627
JessieDu <jiaxin.du@stjlabs.com>
2019-02-12 10:52:11 -0500

    started on minilabs

---
+++ arraylistPractice/src/arraylistPractice/miniLab.java
@@ -0,0 +1,26 @@
+/* Create a method that will receive an integer parameter and then return an ArrayList that 
+ * contains all of the number’s factors, excluding 1 and itself.  
+
+Create a 2nd method that will remove all numbers from its ArrayList parameter that are not 
+composite numbers.  Composite numbers are divisible by 1, itself, and must have at least 1 other 
+positive factor.  You will need to use % to determine if a number is a factor.  While you will 
+want to create a main method for testing, you only need to hand in the completed methods of the 
+class as shown below.
+*/
+package arraylistPractice;
+import java.util.ArrayList;
+
+public class miniLab {
+		
+		public static ArrayList<Integer> getListOfFactors(int number)
+		  {                                                                        	
+			ArrayList<Integer> factors = new ArrayList<Integer>(); 
+			
+		  }
+		 
+		  public static void keepOnlyCompositeNumbers( ArrayList<Integer> nums )
+		  {
+		  	// you will add code	
+		  }
+
+}
