296b0d3c923e7671ac98ab5907a8cb38c5a26e35
JessieDu <jiaxin.du@stjlabs.com>
2019-02-12 10:45:18 -0500

    finish some practice about arrayList today

--- arraylistPractice/src/arraylistPractice/Practice.java
+++ arraylistPractice/src/arraylistPractice/Practice.java
@@ -7,16 +7,15 @@ public class Practice {
 	public static void main(String[] args) {
 		ArrayList<String> band = new ArrayList<String>();
 		
-		band.add("Lindsey");
-	  	band.add("Mick");
-		band.add("Stevie");
-		band.add("Dave");
-		band.add("Christine");
-		band.add(band.remove(2));
-		band.set(0,band.get(4));
+		band.add("Paul"); 
+		band.add("Ringo");
+		band.add("John"); 
+		band.add("George"); 
+		band.add(band.remove(0));
 
-		
-		System.out.println(band);
+		System.out.println ("Band: " + band); 
+		System.out.println ("Size of the band: " + band.size()); 
+
 
 	}
 
