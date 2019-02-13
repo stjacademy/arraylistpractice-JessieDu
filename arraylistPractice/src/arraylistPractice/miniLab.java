0727d09971d74502e1c75fab36315c2c6f132af5
JessieDu <jiaxin.du@stjlabs.com>
2019-02-13 11:33:52 -0500

    finished the minilab

--- arraylistPractice/src/arraylistPractice/miniLab.java
+++ arraylistPractice/src/arraylistPractice/miniLab.java
@@ -8,19 +8,41 @@ want to create a main method for testing, you only need to hand in the completed
 class as shown below.
 */
 package arraylistPractice;
+
 import java.util.ArrayList;
 
 public class miniLab {
-		
-		public static ArrayList<Integer> getListOfFactors(int number)
-		  {                                                                        	
-			ArrayList<Integer> factors = new ArrayList<Integer>(); 
-			
-		  }
-		 
-		  public static void keepOnlyCompositeNumbers( ArrayList<Integer> nums )
-		  {
-		  	// you will add code	
-		  }
 
+	public static ArrayList<Integer> getListOfFactors(int number) {
+		ArrayList<Integer> factors = new ArrayList<Integer>();
+		for (int j = 2; j < number; j++) {
+			if (number % j == 0) {
+				factors.add(j);
+			}
+		}
+		return factors;
+	}
+
+	public static void keepOnlyCompositeNumbers(ArrayList<Integer> nums) {
+		for (int i = 0; i<nums.size(); i++) {
+			if (getListOfFactors(nums.get(i)).size()<=0)
+				nums.remove(i);
+			i--;
+		}
+	}
+
+	public static void main(String args[]) {
+		ArrayList<Integer> composite = new ArrayList<Integer>();
+		composite.add(2);
+		composite.add(3);
+		composite.add(6);
+		composite.add(127);
+		composite.add(254);
+		composite.add(381);
+		
+		System.out.println(getListOfFactors(9));
+		System.out.println(getListOfFactors(23));
+		System.out.println(getListOfFactors(762));
+		keepOnlyCompositeNumbers(composite);
+	}
 }
